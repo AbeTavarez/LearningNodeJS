9da43ddde55a621a5c6fc5c80a6e6d1a6eea720b
Abraham E. Tavarez <abeincwireless@gmail.com>
2021-12-05 19:12:56 -0500

    notes app init

---
+++ notes-app/app.js
@@ -0,0 +1,8 @@
+
+
+// loading in the file system module
+const fs = require('fs');
+
+// fs.writeFileSync('my_notes.txt', "My first note!");
+
+fs.appendFileSync('my_notes.txt', " My new text data!!!!")
