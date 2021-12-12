8d9a0ce7bcec50889b100e8ca2678b547a1d60c4
Abraham E. Tavarez <abeincwireless@gmail.com>
2021-12-12 15:09:43 -0500

    conditional rendering

--- mvc_app/views/Show.jsx
+++ mvc_app/views/Show.jsx
@@ -1,11 +1,28 @@
-const React = require('react');
+const React = require("react");
 
 class Show extends React.Component {
-    render() {
-        return (
-            <h1>Show Page</h1>
-        )
-    }
-};
+  render() {
+    const { tvshow } = this.props; // we access our props
+    const { url } = this.props;
+    console.log(url);
+    return (
+      <div>
+        <h1>Show Page</h1>
+        {
+            url === '/tvshows' ? <div>
+            <h2>
+              The ${tvshow.title} came out in {tvshow.year}.
+            </h2>
+            <p>
+              {tvshow.isFavorite
+                ? `${tvshow.title} is one of my favorite tv shows.`
+                : null}
+            </p>
+          </div> : null
+        }
+      </div>
+    );
+  }
+}
 
 module.exports = Show;
