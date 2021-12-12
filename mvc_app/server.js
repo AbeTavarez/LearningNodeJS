14f0654fd5a1967b6c36e6a18d2644addd75dc73
Abraham E. Tavarez <abeincwireless@gmail.com>
2021-12-12 14:12:36 -0500

    sever middlewares

--- mvc_app/server.js
+++ mvc_app/server.js
@@ -1,8 +1,16 @@
-import express from 'express';
+const express = require('express');
 
 const app = express();
 
+//* ===== Setup our Engine
+app.set('view engine', 'jsx');
+app.engine('jsx', require('express-react-views').createEngine());
+
+//* ===== Routes
+app.get('/', (req, res) => {
+    res.render('Show')
+});
 
 const PORT = 3001;
 
-app.listen(()=> console.log(`Now listen on ${PORT}...`))
+app.listen(PORT,()=> console.log(`Now listening on ${PORT}...`))
