cfce68f392625f726c2c3c394c6db881cfa5ab36
Abraham E. Tavarez <abeincwireless@gmail.com>
2021-12-12 13:35:15 -0500

    extracted data

---
+++ mvc_app/models/data.js
@@ -0,0 +1,18 @@
+export const tvshows = [
+    {
+        title: "silicon valley",
+        year: 2010,
+        isFavorite: true,
+      },
+      {
+        title: "Mr Robot",
+        year: 2012,
+        isFavorite: true,
+      },
+      {
+        title: "The Office",
+        year: 2005,
+        isFavorite: false,
+      },
+];
+
